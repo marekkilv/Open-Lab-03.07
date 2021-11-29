a224f7063e417cf8aba2edacfb76f3c5c0d2c3ae
Marekmp@outlook.sk <Marekmp@outlook.sk>
2021-11-29 19:43:11 +0100

    mam

--- Open-Lab-03.07/Divider.cs
+++ Open-Lab-03.07/Divider.cs
@@ -6,7 +6,11 @@ namespace Open_Lab_03._07
     {
         public bool EqualSlices(int total, int people, int each)
         {
-            throw new NotImplementedException();
+            if (total = people * each)
+            {
+                return true;
+            }
+            return false;
         }
     }
 }
